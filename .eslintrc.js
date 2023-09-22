e776e43f283cb5ccaf6494116d2d5a4c3f659ee8
Wavpro <albin.hedwall@gmail.com>
2023-09-23 00:44:55 +0200

    base

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    root: true,
+    env: {
+        browser: true,
+        node: true,
+    },
+    parserOptions: {
+        ecmaVersion: 8,
+    },
+    extends: ['eslint:recommended', 'plugin:vue/vue3-recommended', 'prettier'],
+    plugins: ['prettier'],
+    rules: {
+        'vue/require-default-prop': 0,
+        'vue/html-indent': ['error', 2],
+        'vue/singleline-html-element-content-newline': 0,
+        'vue/component-name-in-template-casing': ['error', 'PascalCase'],
+        'vue/multi-word-component-names': 0,
+    },
+    globals: {
+        _: true,
+    },
+}
